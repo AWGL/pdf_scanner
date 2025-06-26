cc14b32f13d3522bc94ad2644f2eee0d33362d39
AntWarland <ant.warland@gmail.com>
2025-06-26 10:34:56 +0100

    now outputs to text file and to the ./output directory. Argument for filename added

--- readme.md
+++ readme.md
@@ -1,25 +1,28 @@
-This script :
+### PDF_scanner
 
 * converts all PDF files in a specified directory to pillow image objects
 * extracts text from the images using tesseract
 * searches the text for a given string pattern
-* prints the filename and page number to the terminal if the pattern is found
+* prints the filename and page number to a text file with a given filename if the pattern is found
 
-usage: python run.py [input_dir] [pattern]
+usage: `python run.py [input_dir] [output_dir] [pattern]`
 
 Make sure to adjust the directory paths as needed.
 
-Installation:
+**Installation:**
 
-conda create --name pdf_stringfinder pdf2image pillow pytesseract click
+`conda create --name pdf_scanner pdf2image pillow pytesseract click
+
+conda activate pdf_scanner`
 
 ensure you have poppler installed on your system using the command: 
 
-sudo apt install poppler-utils
+`sudo apt install poppler-utils`
+
+---
 
 Note that the pattern is case-sensitive
-See example output below:
 
-![Screenshot from 2025-06-25 17-01-51](https://github.com/user-attachments/assets/64b45d78-c775-4a86-b5ed-74f24dbe9c01)
+See example output "search_shimano.txt" in ./output
 
-Test PDFs can be found in the test_pdfs folder
+Test PDFs can be found in ./test_pdfs
--- run.py
+++ run.py
@@ -7,25 +7,28 @@ import click
 
 @click.command()
 @click.argument('input_dir', type=click.Path(), default=".")#, help='Path to the directory containing PDF files. Default is current directory.')
+@click.argument('output_file_name', type=click.Path(), default="search_results.txt")#, help='Path to the directory to save output text file. Default is current directory.')
 @click.argument('pattern', type=click.STRING, default=None)#, help='string or pattern to seach for in the PDF files')
 
-def main(input_dir, pattern):
+def main(input_dir, output_file_name, pattern):
+    
     # Get all PDF files
     pdf_files = glob.glob(os.path.join(input_dir, "*.pdf"))
-    print(f"Found {len(pdf_files)} PDF files.")
+    with open(f"./output/{output_file_name}", "w") as f:
+        f.write(f"Found {len(pdf_files)} PDF files. \n")
 
-    for file in pdf_files:
-        
-        print(f"Processing file: {file}")
-        filename = os.path.splitext(os.path.basename(file))[0]
-        images = convert_from_path(file)  # Add poppler_path=... if needed
-        
-        for i, image in enumerate(images):
+        for file in pdf_files:
             
-            image_filename = f"{filename} page {i + 1}"
-            page_string = pytesseract.image_to_string(image)
-            if pattern in page_string:
-                print(f"Pattern '{pattern}' found in {image_filename}.")
+            f.write(f"Processing file: {file} \n")
+            filename = os.path.splitext(os.path.basename(file))[0]
+            images = convert_from_path(file)  # Add poppler_path=... if needed
+            
+            for i, image in enumerate(images):
+                
+                image_filename = f"{filename} page {i + 1}"
+                page_string = pytesseract.image_to_string(image)
+                if pattern in page_string:
+                    f.write(f"    Pattern '{pattern}' found in {image_filename}.\n")
 
 if __name__ == "__main__":
     main()
